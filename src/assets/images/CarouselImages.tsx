b097d26fe1eaed6c5ef29c2ccd341d4c1ff6f2d8
John <wheeler6123@gmail.com>
2023-09-23 10:38:30 -0600

    created CarouselImages source file

---
+++ src/assets/images/CarouselImages.tsx
@@ -0,0 +1,39 @@
+import avocadotoast from './avocadotoast.jpg';
+import bar from './bar.jpg';
+import breadbasket from './breadbasket.jpg';
+import chefplating from './chefplating.jpg';
+import foodontable1 from './foodontable1.jpg';
+import girleating from './girleating.jpg';
+import platedfood from './platedfood.jpg';
+
+export const CarouselImages = [
+    {
+        id: 1,
+        src: avocadotoast,
+    },
+    {
+        id: 2,
+        src: bar,
+    },
+    {
+        id: 3,
+        src: breadbasket,
+    },
+    {
+        id: 4,
+        src: chefplating,
+    },
+    {
+        id: 5,
+        src: foodontable1,
+    },
+    {
+        id: 6,
+        src: girleating,
+    },
+    {
+        id: 7,
+        src: platedfood,
+    },
+];
+
