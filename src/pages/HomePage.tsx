8bd2ead086a727f4fadde8e3a65223428ac5a003
John <wheeler6123@gmail.com>
2023-09-25 11:08:48 -0600

    added ResponsiveCarousel component to HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PhotoCarousel from "../components/PhotoCarousel";
+import ResponsiveCarousel from "../components/ResponsiveCarousel";
+
+
+const HomePage = () => {
+
+    return (
+        <div className="home-page-body">
+            <ResponsiveCarousel />
+        </div>
+    )
+}
+
+export default HomePage
