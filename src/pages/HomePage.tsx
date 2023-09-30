9dc26aee067c70257a0243afb59b12e8d59c2d79
John <wheeler6123@gmail.com>
2023-09-30 13:26:37 -0600

    built contact section of homepage, applied css styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,6 +22,51 @@ const HomePage = () => {
                     </p>
                 </div>
             </div>
+            <div className="contact-container">
+                <div className="contact-left">
+                    <div className="contact-left-text">
+                        <p>location</p>
+                        <p>114 W. Houston St.</p>
+                        <p>Philadelphia, PA 19130</p>
+                        <br />
+                        <br />
+                        <p>contact</p>
+                        <p>(215)856-3654</p>
+                        <p>hello@madoline.com</p>
+                        <br />
+                        <br />
+                        <p>hours</p>
+                        <p>Monday</p>
+                        <p>Closed</p>
+                        <p>Tuesday - Thurday</p>
+                        <p>11am - 10pm</p>
+                        <p>Friday - Saturday</p>
+                        <p>10am - 11pm</p>
+                        <p>Sunday</p>
+                        <p>10am - 8pm</p>
+                    </div>
+                </div>
+                <div className="contact-right">
+                    <form action="submit" className="contact-form">
+                        <h2>contact us</h2>
+                        <div className="contact-name">
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div className="contact-email">
+                            <label htmlFor="email">Email:</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div className="contact-message">
+                            <label htmlFor="message">Message:</label>
+                            <textarea name="message" id="message" cols={30} rows={10}></textarea>
+                        </div>
+                        <div className="contact-submit">
+                            <input type="submit" value="Send" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
