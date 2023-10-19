5cda6949bbdd7f4ab4fa7d1321ae7fe5655dfa79
John <wheeler6123@gmail.com>
2023-10-19 09:11:08 -0600

    created MenuPage, added Route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import '../src/assets/style/App.css'
 import HomePage from './pages/HomePage';
+import MenuPage from './pages/MenuPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
       </Routes>
     </Router>
   )
--- src/components/ResponsiveCarousel.tsx
+++ src/components/ResponsiveCarousel.tsx
@@ -7,7 +7,7 @@ import { CarouselImages } from "../assets/images/CarouselImages";
 const ResponsiveCarousel = () => {
 
     return (
-        <Carousel className="crsl" autoPlay infiniteLoop transitionTime={1000} showArrows={false} showIndicators={false} showThumbs={false}>
+        <Carousel className="crsl" autoPlay infiniteLoop interval={2000} transitionTime={1000} showArrows={false} showIndicators={false} showThumbs={false}>
             {CarouselImages.map((image) => (<img key={image.id} src={image.src} alt="carousel slide" />))}
         </Carousel>
     )
