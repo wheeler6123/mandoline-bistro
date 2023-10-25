69ec543289b2b461f462a781f7f569965ab848d1
John <wheeler6123@gmail.com>
2023-10-25 13:42:41 -0600

    updated carousel to remove status property

--- src/components/ResponsiveCarousel.tsx
+++ src/components/ResponsiveCarousel.tsx
@@ -7,7 +7,7 @@ import { CarouselImages } from "../assets/images/CarouselImages";
 const ResponsiveCarousel: React.FC = () => {
 
     return (
-        <Carousel className="crsl" autoPlay infiniteLoop interval={2000} transitionTime={1000} showArrows={false} showIndicators={false} showThumbs={false}>
+        <Carousel className="crsl" autoPlay infiniteLoop interval={2000} transitionTime={1000} showArrows={false} showIndicators={false} showThumbs={false} showStatus={false}>
             {CarouselImages.map((image) => (<img key={image.id} src={image.src} alt="carousel slide" />))}
         </Carousel>
     )
